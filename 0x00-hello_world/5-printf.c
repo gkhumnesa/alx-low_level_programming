775b55fd34d67457761a7d32ac0cb5415a12fb1d
gifti <gkhumnesa123@gmail.com>
2022-07-03 03:50:32 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*
+ * about the main and printf
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/*
+ * the main function returns the values ot sife of
+ * all data types
+ */
 int main(void)
 {
-printf("size of char: %d byte\n",sizeof(char));
-printf("size of int: %d byte\n",sizeof(int));
-printf("size of long int: %d byte\n",sizeof(long int));
-printf("size of float: %d byte\n",sizeof(float));
+printf("size of char: %d byte\n", sizeof(char));
+printf("size of int: %d byte\n", sizeof(int));
+printf("size of long int: %d byte\n", sizeof(long int));
+printf("size of float: %d byte\n", sizeof(float));
 return (0);
 }
